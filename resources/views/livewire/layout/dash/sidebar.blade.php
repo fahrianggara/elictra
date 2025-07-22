891e64150e326df96c2ade1f3308d917d8289831
Fahri Anggara <fahriangga30@gmail.com>
2025-07-22 20:32:26 +0700

    Refactor sidebar: comment out unused navigation items for better clarity and maintenance

--- resources/views/livewire/layout/dash/sidebar.blade.php
+++ resources/views/livewire/layout/dash/sidebar.blade.php
@@ -25,13 +25,13 @@
         <li class="nav-title">Manajemen</li>
         <x-dash.nav-item href="#" icon="fas fa-file-invoice">Tagihan</x-dash.nav-item>
         <x-dash.nav-item href="#" icon="fas fa-credit-card">Pembayaran</x-dash.nav-item>
-        <x-dash.nav-item href="#" icon="fas fa-ban">Pelanggan Diblokir</x-dash.nav-item>
+        {{-- <x-dash.nav-item href="#" icon="fas fa-ban">Pelanggan Diblokir</x-dash.nav-item> --}}
 
-        <li class="nav-title">Laporan</li>
-        <x-dash.nav-item href="#" icon="fas fa-chart-line">Laporan</x-dash.nav-item>
+        {{-- <li class="nav-title">Laporan</li>
+        <x-dash.nav-item href="#" icon="fas fa-chart-line">Laporan</x-dash.nav-item> --}}
 
         <li class="nav-title">Pengaturan</li>
         <x-dash.nav-item href="#" icon="fas fa-cogs">Pengaturan</x-dash.nav-item>
-        <x-dash.nav-item href="#" icon="fas fa-history">Log Aktivitas</x-dash.nav-item>
+        {{-- <x-dash.nav-item href="#" icon="fas fa-history">Log Aktivitas</x-dash.nav-item> --}}
     </ul>
 </aside>
